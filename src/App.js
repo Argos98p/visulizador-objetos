45f0d012b43015bb5a2c00ab55b574c334df3555
argos98 <jaio1998619@gmail.com>
2022-07-18 17:16:19 -0500

    otros visualizadores

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import './App.css';
-import React360Viewer from "react-360-view";
-function App() {
-  var visualizerContainer={width:"38%", };
-  return (
-    <div className="container">
-      <div className="row" style={{textAlign:"center"}}>
-        <div className="col-12 mb-4 card p-0"  style={{textAlign:"center", margin: "auto",
-  width: "65%"}}>
-          
-          <div className="card" style={visualizerContainer}>
-          <React360Viewer
-            amount={114}
-            imagePath={`./img/`}
-            fileName="control_{index}.jpg"
-            buttonClass="ligth"
-          />
-          </div>
-          
+import React360Viewer from "react-360-view-simple";
+//import ThreeSixty from 'react-360-view-lilith';
+import ThreeSixty from 'react-360-view-extended';
+function App() { // var visualizerContainer={width:"42%", };
+    return (
+
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-6 card p-0">
+                    <div className="v360-header text-light bg-dark">
+                        <span className="v360-header-title">36 Images - Autoplay (1 loop) - Reverse Spin</span>
+                        <span className="v360-header-description"></span>
+                    </div>
+
+                    <React360Viewer amount={73}
+                        imagePath={`./carro/`}
+                        fileName="carro_{index}.png"
+                        loop={1}
+                        //buttonClass="ligth"
+                    />
+
+                    <ThreeSixty amount={73}
+                        imagePath={`./carro/`}
+                        fileName="carro_{index}.png"
+                        loop={1}
+                        //buttonClass="ligth"
+                    />
+
+                    <ThreeSixty amount={73}
+                        imagePath={`./carro/`}
+                        fileName="carro_{index}.png"
+                        loop={1}
+                        //buttonClass="ligth"
+                    />
+
+
+                </div>
+            </div>
         </div>
-      </div>
-      
-    </div>
-  );
+
+
+    );
 }
 
 export default App;
