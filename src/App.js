7f12810c633abec32030b6e7566e9fe58f777de8
argos98 <jaio1998619@gmail.com>
2022-10-11 17:26:13 -0500

    update

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
 
 
             <Routes>
-                <Route index element={<h1>hola</h1>} />
+                <Route index element={<h1>index</h1>} />
 
-                <Route path="/visualizador/:id/*" element={<Controller editMode={false}/>} />
+                <Route path="/visualizador/view/:id/*" element={<Controller editMode={false}/>} />
                 <Route path="/visualizador/edit/:id/*" element={<Controller editMode={true}/>} />
-                <Route path="*" element={<h1>hola 2</h1>} />
+                <Route path="*" element={<h1>404</h1>} />
             </Routes>
 
 
