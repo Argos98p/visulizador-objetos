0ce8c462838bedd6adc8721ff66901c3be543a4b
argos98 <jaio1998619@gmail.com>
2023-01-30 12:09:28 -0500

    update index

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,29 @@
 
      Link
  } from "react-router-dom"
+ import Timeline from "./timeline/Timeline";
 const Home =  () =>{
     return (
         <>
             <div className={"home-page-container"}>
-                <div className={"image-container"}>
+                <h1 className={"Titulo blanco weigth"}>¿Como vender tu auto?</h1>
+                <div className={"image-container3"}>
+                    <img className={"images-marketa"} src={"../../auto-marketa.png"} alt={"e"}/>
+                </div>
+
+                <Timeline></Timeline>
+                {
+                    /*
+                    * <div className={"image-container"}>
                     <img src={"../../Motors_1PNG Grande.png"} alt={"e"}/>
                 </div>
                 <div className={"image-container2"} onClick={()=>{window.location.href = 'https://play.google.com/store/apps/details?id=com.threedspaceinc.appmotors';
                 }}>
                     <img src={"../../getonplay.png"} alt={"fe"}/>
                 </div>
+                    * */
+                }
+
             </div>
         </>
     );
