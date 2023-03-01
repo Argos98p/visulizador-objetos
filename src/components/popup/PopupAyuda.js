abad305a556c83f00ca18323e833e73ea5e1a61e
argos98 <jaio1998619@gmail.com>
2023-03-01 16:31:36 -0500

    update

--- src/components/popup/PopupAyuda.js
+++ src/components/popup/PopupAyuda.js
@@ -14,13 +14,36 @@ import {useNavigate} from "react-router-dom";
 const AyudaPopup=()=>{
     const navigate = useNavigate();
      return <Popup
-        className="popup-compartir_container"
+        className="popup-ayuda_container"
         onClose={()=>{navigate(-1)}}
         open={true}
         modal
         nested >
         <div className="popup-ayuda-relative">
-            <h1>hola</h1>
+            <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+                <div className={"my-ayuda-container"}>
+                    <div className={"ayuda-row"}>
+                        <p>Haga click o toque la pantalla y arrastre para mirar el auto en 3d</p>
+                        <img src={"/hand.png"}/>
+
+                    </div>
+                    <div className={"ayuda-row"}>
+                        <p>Toque los hotspots para ver mas información del auto</p>
+                        <img src={"/hand1.png"}/>
+
+                    </div>
+                    <div className={"ayuda-row"}>
+
+                        <p>Utilize la rueda del mouse o un gesto con dos dedos para acercar y alejar</p>
+                        <img src={"/zoom-in.png"}/>
+
+                    </div>
+                </div>
+                <button className={"mi-boton"} onClick={()=>{navigate(-1)}} style={{width:"100px"}}>Cerrar</button>
+
+            </div>
+
+
         </div>
 
     </Popup>
