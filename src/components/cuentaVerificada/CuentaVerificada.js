cd35e547bc0e0b63bb3b032d296b49051ad07102
argos98 <jaio1998619@gmail.com>
2023-03-17 15:54:25 -0500

    update cuenta verificada

--- src/components/cuentaVerificada/CuentaVerificada.js
+++ src/components/cuentaVerificada/CuentaVerificada.js
@@ -7,7 +7,7 @@ const CuentaVerificada = () => {
             </div>
             <div>
                 <h1 className={"text-center2"}>Tu Cuenta está verificada</h1>
-                <p>Aquí te mostramos como funciona la aplicación 3D MOTOR's para que puedas sacarle el máximo provecho</p>
+                <p className={"text-center2"}>Aquí te mostramos como funciona la aplicación 3D MOTOR's para que puedas sacarle el máximo provecho</p>
                 <p className={"text-center2"}><a href={"https://www.youtube.com/watch?v=ouxXyP89rcc"}>Ir al link de Youtube</a> </p>
             </div>
         </>
